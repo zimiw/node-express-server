c65720ba9855be0edcbc6184a6236935aaff7da0
yongyuehuang <1130216245@qq.com>
2016-12-15 18:55:56 +0800

    json

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var fs = require("fs");
 
 var nav = require('../data/navigation.json')
+var bookList = require('../data/bookList.json')
 
 /* model */
 var Navigation = require('../models/Navigation')
@@ -16,6 +17,10 @@ router.get('/book/navigation', function (req, res, next) {
     nav.push()
   }
   return res.send(nav);
+})
+
+router.get('/book/list', function (req, res, next) {
+  return res.send(bookList);
 })
 
 module.exports = router;
